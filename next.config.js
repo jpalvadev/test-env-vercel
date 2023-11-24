580a2a836984339703ca2a4d7fe2defa7f4a9f53
Juan Pablo Alvarez <jpalvadev@gmail.com>
2023-11-24 11:53:36 -0300

    fix env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_NOWPAYMENTS_API_KEY:
+      process.env.NEXT_PUBLIC_NOWPAYMENTS_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
